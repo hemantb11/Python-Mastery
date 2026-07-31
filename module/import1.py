07a10550f43c7962c19a29bece95aa357916adfe
Hemant Badgujar <hemantbadgujar2006@gmail.com>
2026-07-31 12:42:26 +0530

    Add import1.py to demonstrate function calls

---
+++ module/import1.py
@@ -0,0 +1,9 @@
+ # call entire function 
+import import2
+print(import2.add(10,40))
+print(import2.sub(30,10))
+
+ # call limited function
+from import2 import add,sub
+print(add(10,40))
+print(sub(30,10))
